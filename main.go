25748626ef66f97cb7c7a7bd75db10478e4c05e4
ZUCCARINI ALESSANDRO <alex2001zu@gmail.com>
2026-02-20 12:44:44 +0100

    add: lesson 1.5 - list of commands help and exit

--- main.go
+++ main.go
@@ -7,19 +7,59 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 
 	for true {
 		fmt.Print("Pokedex >")
 		scanner.Scan()
 		input := scanner.Text()
 
-		result := cleanInput(input)[0]
+		command, ok := commands[input]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
 
-		fmt.Printf("Your command was: %s\n", result)
-
+		command.callback()
 	}
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Print("Usage:\n\n")
+
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
 }
 
 func cleanInput(text string) []string {
